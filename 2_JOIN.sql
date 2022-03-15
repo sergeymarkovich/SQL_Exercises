a301a9d22a111a85df237e751c6275ed05e48cf0
Sergey_Markin <sergey-markin2013@mail.ru>
2022-03-15 20:12:40 +0300

    15.03

--- 2_JOIN.sql
+++ 2_JOIN.sql
@@ -337,4 +337,25 @@ INSERT INTO book(title, author_id, price, amount)
 SELECT * FROM book;
 
 /*Запрос на обновление, вложенные запросы
+Занести для книги «Стихотворения и поэмы» Лермонтова жанр «Поэзия», а для книги «Остров сокровищ» Стивенсона - «Приключения». (Использовать два запроса)
+*/
 
+UPDATE book
+SET genre_id = 
+(
+    SELECT genre_id
+    FROM genre 
+    WHERE name_genre = 'Поэзия'
+)
+WHERE title = 'Стихотворения и поэмы';
+
+UPDATE book
+SET genre_id = 
+(
+    SELECT genre_id
+    FROM genre 
+    WHERE name_genre = 'Приключения'
+)
+WHERE title = 'Остров сокровищ';
+
+SELECT * FROM book;
